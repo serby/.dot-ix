c9d94dc413cc45b1be2c47e3ea0df208bf86cb7d
Paul Serby <paul.serby@clock.co.uk>
2019-08-14 16:35:46 +0100

    Loads of new goodness!

--- .mongorc.js
+++ .mongorc.js
@@ -1,45 +1,73 @@
 /* global db */
 
-db.getMongo().getDBNames().forEach(function(database) {
-	if (/^_?test/.test(database)) db.getMongo().getDB(database).dropDatabase()
-})
+load('/Users/serby/.dot-ix/lodash.min.js')
 
-DBCollection.prototype.read = function (id) {
+function toCsv(objArray, headers = []) {
+  const array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray
+  const properties = Object.keys(array[0]).filter(
+    value => headers.length === 0 || headers.includes(value)
+  )
+  let str = `${properties.map(value => `"${value}"`).join('\t')}` + '\r\n'
+
+  return array.reduce((str, next) => {
+    str += `${properties.map(value => `"${next[value]}"`).join('\t')}` + '\r\n'
+    return str
+  }, str)
+}
+
+db.getMongo()
+  .getDBNames()
+  .forEach(function(database) {
+    if (/^_?test/.test(database))
+      db.getMongo()
+        .getDB(database)
+        .dropDatabase()
+  })
+
+DBCollection.prototype.read = function(id) {
   return this.find({ _id: ObjectId(id) })
 }
 
+DBCollection.prototype.search = function(terms) {
+  return this.find({ $text: { $search: terms } })
+}
+
 function countDistinct(collectionName, propertyName) {
-	db[collectionName].aggregate([{ $group: { _id: '$' + propertyName}  },{ $group: { _id: 1, count: { $sum: 1 } } } ])
+  return db[collectionName].aggregate([
+    { $group: { _id: '$' + propertyName } },
+    { $group: { _id: 1, count: { $sum: 1 } } }
+  ])
 }
 
 function sortObject(obj, reverse) {
-  var output = []
-  	, prop
+  var output = [],
+    prop
   for (prop in obj) {
     if (obj.hasOwnProperty(prop)) {
-    	output.push({ key: prop, value: obj[prop] })
+      output.push({ key: prop, value: obj[prop] })
     }
   }
   if (reverse) {
-  	output.sort(function(a, b) {
-  		return b.value - a.value
-  	})
+    output.sort(function(a, b) {
+      return b.value - a.value
+    })
   } else {
-  	output.sort(function(a, b) {
-  		return a.value - b.value
-  	})
+    output.sort(function(a, b) {
+      return a.value - b.value
+    })
   }
   return output
 }
 
 function printArray(list) {
-	list.forEach(function (row) {
-		print(row.value, row.key)
-	})
+  list.forEach(function(row) {
+    print(row.value, row.key)
+  })
 }
 
-var clock =
-	{ countDistinct: countDistinct
-	, sortObject: sortObject
-	, printArray: printArray
-	}
+var clock = {
+  countDistinct: countDistinct,
+  sortObject: sortObject,
+  printArray: printArray,
+  toCsv: toCsv
+}
